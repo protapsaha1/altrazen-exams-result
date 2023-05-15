ba276dca065a86cc39b20553623b5f58b82a19da
protapsaha1 <webprotap09@gmail.com>
2023-05-15 17:04:32 +0600

    polytechnic results

--- src/components/Pass/Pass.jsx
+++ src/components/Pass/Pass.jsx
@@ -6,7 +6,7 @@ const Pass = () => {
             <img className='w-[1238px] h-[740px] mx-auto my-16 rounded-lg' src={pass} alt="" />
             <h1 className='text-center font-bold text-8xl'>Your Results</h1>
             <div className='w-[1200px] h-96 border shadow-2xl rounded-xl mx-auto mt-5 bg-red-400'>
-
+                <h1>C-GPA : 4</h1> 
             </div>
         </div>
     );
--- src/components/Refer/Refer.jsx
+++ src/components/Refer/Refer.jsx
@@ -6,8 +6,9 @@ const Refer = () => {
         <div className='w-[1980px] mx-auto'>
             <img className='w-[1238px] h-[740px] mx-auto my-16 rounded-lg' src={refer} alt="" />
             <h1 className='text-center font-bold text-8xl'>Your Results</h1>
-            <div className='w-[1200px] h-96 border shadow-2xl rounded-xl mx-auto mt-5 bg-red-400'>
-
+            <div className='w-[1200px] h-96 border shadow-2xl rounded-xl mx-auto mt-5 bg-red-400 p-10'>
+                <h1 className='text-5xl font-bold mb-3'>Physics-2 <span>fail</span> </h1>
+                <h1 className='text-5xl font-bold'>Mathematics-3 <span>fail</span></h1>
             </div>
         </div>
     );
